45fba49ccaebefb476491baf5fad44acbc2ae86f
Cosmin Gherghe <hello@cosmingherghe.dev>
2021-01-31 09:59:01 +0000

    Initial commit

---
+++ src/main/java/dev/cosmingherghe/pricealertwishlist/PriceAlertWishlistApplication.java
@@ -0,0 +1,13 @@
+package dev.cosmingherghe.pricealertwishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceAlertWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceAlertWishlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cosmingherghe/pricealertwishlist/PriceAlertWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cosmingherghe.pricealertwishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceAlertWishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
